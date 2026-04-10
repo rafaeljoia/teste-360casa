8cf1c601c2d8fc87be951dbf30f4c039231ca6c8
Rafael Joia <rafael.joia@fgv.br>
2026-04-10 15:57:55 -0300

    primeiro commit

---
+++ app-files/data.js
@@ -0,0 +1,150 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-foto_entrada",
+      "name": "foto_entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.0997218571332077,
+        "pitch": -7.105427357601002e-15,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0761610290688868,
+          "pitch": 0.585174551736527,
+          "rotation": 6.283185307179586,
+          "target": "1-entrada-sala"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3749251085733984,
+          "pitch": -0.1331048737531617,
+          "title": "Casa",
+          "text": "Rua São Vicente"
+        }
+      ]
+    },
+    {
+      "id": "1-entrada-sala",
+      "name": "entrada-sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.7088173900956605,
+        "pitch": 0.392973412545242,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.480033340181345,
+          "pitch": 0.22190029323469496,
+          "rotation": 18.84955592153877,
+          "target": "2-sala"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sala",
+      "name": "sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.05699034292226912,
+        "pitch": -0.022527186345413952,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.2686877703779338,
+          "pitch": -0.17805547439484926,
+          "title": "TV",
+          "text": "SMART 48"
+        },
+        {
+          "yaw": 0.3303541688516489,
+          "pitch": 0.1386160279473625,
+          "title": "Playstation 4",
+          "text": "Com jogos"
+        }
+      ]
+    }
+  ],
+  "name": "Tour Casa",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
